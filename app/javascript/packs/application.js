da2dd2c7c3b91fb6172cc54a98e66f61394ec0cc
James Devereux <devereuxjj@gmail.com>
2019-12-30 10:46:21 +0700

    started bootstraping layouts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,6 +19,9 @@ require("channels")
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
+import "bootstrap";
+import "../stylesheets/application";
+
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
